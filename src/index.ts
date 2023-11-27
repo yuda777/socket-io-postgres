74f6d8f33d1f3437f6b40b8701d04352f233dc22
yuda <zerone777@gmail.com>
2023-11-27 14:52:33 +0700

    set ts

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { Pool } from "pg"; // Import PostgreSQL's Pool from 'pg'
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// PostgreSQL connection setup
+const pool = new Pool({
+	user: "your_username",
+	host: "your_host",
+	database: "your_database",
+	password: "your_password",
+	port: 5432,
+});
+
+// Example query
+pool.query("SELECT NOW()", (err, res) => {
+	console.log(err, res);
+	// You can handle the result here if needed.
+});
+
+// Socket.IO setup
+io.on("connection", (socket) => {
+	console.log("A user connected");
+	// Handle socket events here.
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
